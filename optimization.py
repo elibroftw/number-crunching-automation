2c3214c9dc25040943be0fc26e63c8f897f95ba6
Elijah Lopez <elijahllopezz@gmail.com>
2021-12-17 21:56:55 -0500

    Add files via upload

---
+++ optimization.py
@@ -0,0 +1,12 @@
+"""
+===============================================================
+This is a python script containing various utilities for school
+Made by Elijah Lopez
+===============================================================
+"""
+import numpy as np
+
+
+def inverse(mat):
+    return np.linalg.inv(mat)
+
